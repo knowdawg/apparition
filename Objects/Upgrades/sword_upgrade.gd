6f22a3f4b71eb7fb96e05f79dcde6d1ae6644e15
Nolan Kellerman <nolan@sharkus.com>
2024-07-15 17:58:22 -0700

    Weapon upgrades + controler suport

---
+++ Objects/Upgrades/sword_upgrade.gd
@@ -0,0 +1,52 @@
+extends Node2D
+
+@export var isHilt = true
+@export var room : Level
+var colected = false
+
+@onready var id : String = room.get_name() + get_name()
+func _ready():
+	if isHilt:
+		$Hilt.visible = true
+		$Blade.visible = false
+	else:
+		$Hilt.visible = false
+		$Blade.visible = true
+		
+	if Game.weaponUpgradeStatus.has(id):
+		if Game.weaponUpgradeStatus[id] == true:
+			colected = true
+			$AnimationPlayer.play("RESET")
+			$Hilt.visible = false
+			$Blade.visible = false
+	else:
+		Game.weaponUpgradeStatus[id] = false
+		$AnimationPlayer.play("Idle")
+
+func _on_area_2d_body_entered(body):
+	if body is Player and colected == false:
+		Game.weaponUpgradeStatus[id] = true
+		colected = true
+		$AnimationPlayer.play("Colect")
+		if isHilt:
+			$CanvasLayer/Outline/Blade.visible = hasOtherUpgrade()
+			$CanvasLayer/Outline/Blade2.visible = hasOtherUpgrade()
+			$CanvasLayer/Outline/Hilt.visible = false
+		else:
+			$CanvasLayer/Outline/Blade.visible = false
+			$CanvasLayer/Outline/Blade2.visible = false
+			$CanvasLayer/Outline/Hilt.visible = hasOtherUpgrade()
+
+func hasOtherUpgrade():
+	for i in Game.weaponUpgradeStatus.values():
+		if i == false:
+			return false
+	Game.playerDmg = 1.5
+	return true
+
+func showUI():
+	if isHilt:
+		$CanvasLayer/Outline/Hilt.visible = true
+	else:
+		$CanvasLayer/Outline/Blade.visible = true
+		$CanvasLayer/Outline/Blade2.visible = true
