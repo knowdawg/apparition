2b7528f0f4fe4911fe6a34bbfa042edab9791752
Nolan Kellerman <nolan@sharkus.com>
2024-07-20 20:21:29 -0700

    Left side finshed decorating

--- Levels/MainAreaLevels/CentralSaveRoomShaderScript.gd
+++ Levels/MainAreaLevels/CentralSaveRoomShaderScript.gd
@@ -1,12 +1,13 @@
 extends ColorRect
 
 @export var originNode : Marker2D
+@export var active = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
-	#var positionOnScreen = originNode.get_global_transform_with_canvas().origin
-	#positionOnScreen.x -= 928
-	#positionOnScreen.y -= 320
-	
-	#material.set_shader_parameter("cameraPos", Vector3(positionOnScreen.x * -0.0002 + 0.5, positionOnScreen.y * -0.0002 + 0.5, -2.9))
+	if active:
+		var positionOnScreen = originNode.get_global_transform_with_canvas().origin
+		#positionOnScreen.x -= 928
+		#positionOnScreen.y -= 320
+		
+		material.set_shader_parameter("cameraPos", Vector3(positionOnScreen.x * -0.0001 + 0.5, positionOnScreen.y * -0.0001 + 0.5, -2.9))
